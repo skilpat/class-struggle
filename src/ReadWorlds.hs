2a2cbec1b7a8ded593f63c193b7927d6bcc266c6
Scott Kilpatrick <skilpat@mpi-sws.org>
2015-04-20 12:20:03 +0200

    Reorganized Ctx into WorldCtx

--- src/ReadWorlds.hs
+++ src/ReadWorlds.hs
@@ -23,19 +23,7 @@ import Maybes
 import Moduleish
 import ReadUtils
 import World
-
--- | A context that maps a Moduleish to its typechecked interface and world.
-data Ctx = Ctx { ctx_map  :: UniqFM (Moduleish, ModIface, World) }
-
--- | A wrapper around the Ghc monad that keeps up with a Ctx as state.
-type CtxM a = StateT Ctx Ghc a
-
-instance Monoid Ctx where
-  mempty = Ctx { ctx_map  = emptyUFM }
-
-  mappend Ctx { ctx_map  = map1 }
-          Ctx { ctx_map  = map2 } =
-    Ctx { ctx_map = plusUFM map1 map2 }
+import WorldCtx
         
 
 -- | Build a Ctx mapping modules to worlds and interfaces, given an absolute
@@ -192,47 +180,4 @@ updateCtxMap mish iface w = do
 
 --ctxHasPkg :: Ctx -> String -> Bool
 --ctxHasPkg Ctx {ctx_pkgs = pnames} pname = elementOfUniqSet pname pnames
-
-
-lookupWorld :: Ctx -> String -> Maybe World
-lookupWorld Ctx {ctx_map = cmap} lookup_str = do
-  let (pstr, ':':mstr) = span (/= ':') lookup_str
-  let pid = stringToPackageId pstr
-  let mname = mkModuleName mstr
-  let mish = mkModule pid mname
-  (_, _, w) <- lookupUFM cmap mish
-  return w
-
-
-
-
-
---- PRINTING -----------------
-
-instance Outputable Ctx where
-  --ppr ctx = text "ctx saw packages:" <+> ppr (uniqSetToList (ctx_pkgs ctx))
-  --          $+$ pprCtxEntries ctx []
-  ppr ctx = pprCtxEntries ctx []
-
-
-pprCtxEntries :: Ctx -> [String] -> SDoc
-pprCtxEntries ctx mods_to_print = sep $ catMaybes maybeEntryDocs
-  where
-    maybeEntryDocs = [ pprEntry mish w | (mish, _, w) <- eltsUFM (ctx_map ctx) ]
-    
-    pprEntry mish w
-      | shouldPrint mish = Just $ ppr mish <+> text "->" <+> pprIslands (w_wimap w)
-      | otherwise        = Nothing
-
-    shouldPrint mish
-      | null mods_to_print                   = True
-      | mishModStr mish `elem` mods_to_print = True
-      | otherwise                            = False
-
-
--- | Given a list of module names and a Ctx, print out the World of any modules
---   whose names occur in the list.
-printCtx :: [String] -> Ctx -> IO ()
-printCtx mods ctx = defaultErrorHandler defaultFatalMessager defaultFlushOut $ do
-  runGhc (Just libdir) $ printSDoc $ pprCtxEntries ctx mods
 
