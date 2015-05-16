9ad60cc923501e999250e8f000e10891bf33962d
Scott Kilpatrick <skilpat@gmail.com>
2015-05-16 14:49:37 +0200

    `calc-worlds` executable optionally prints package worlds' islands
    The command-line option `--islands` for `calc-worlds` will additionally print all the islands in each package's world.

--- calc-worlds/Main.hs
+++ calc-worlds/Main.hs
@@ -1,5 +1,8 @@
 module Main where
 
+import System.Environment
+  ( getArgs )
+
 import ReadUtils
   ( readSandboxPath, readPkgList )
 import ReadWorlds
@@ -7,12 +10,17 @@ import ReadWorlds
 import WorldCtx
   ( printCtx )
 
-
 -- | Given a list of packages from STDIN, read all the worlds
---   for these packages and print them out.
+--   for these packages and print them out. If the `--islands` command-line
+--   option is given, then the output will additionally show the islands within
+--   each package's world.
 main :: IO ()
 main = do
   sandbox <- readSandboxPath
   pkgs <- readPkgList
+
+  args <- getArgs
+  let printIslands = "--islands" `elem` args
+
   ctx <- buildCtx sandbox pkgs
-  printCtx [""] False ctx
+  printCtx [""] printIslands ctx
